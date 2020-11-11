b7a688ec1cb367d15eb7f071c17f9f7374e632c9
GinoAntonel <ginoantonel.ga@gmail.com>
2020-11-11 18:17:19 -0300

    delete empty funct

--- main.cpp
+++ main.cpp
@@ -13,10 +13,6 @@ bool hasAuxiliar(char *nextArg){
     return 0;
 }
 
-void verifyAviliableParameter(int i, char **argv){
-
-}
-
 void readArguments(int argc, char **argv){
     for (int i = 1; i < argc; i++){
         if(argv[i][0] == '-'){
