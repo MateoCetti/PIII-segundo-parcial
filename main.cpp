ff8ec3b0abda345796bb78944985528ada36836a
Mateo P. Cetti <mateocetti@hotmail.com>
2020-11-11 14:18:58 -0300

    Read parameters

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include "string.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+void readArguments(int argc, char **argv){
+    cout << "Cantidad de argumentos: " << argc-1 << "\n";
+    for (int i = 1; i < argc; i++){
+        if(argv[i][0] == '-'){
+            cout << "Argumento " << i << " " << argv[i];
+            if(i < argc-1){
+                if(argv[i+1][0] != '-'){
+                    cout<< " Auxiliar: "<< argv[i+1]<<"\n";
+                    i++;
+                }
+            }
+            cout<< "\n\n";
+        }
+        else{
+            cout<<"Ingrese un argumento valido.\n";
+        }
+    }
+    cout << "\n";
+}
+
+int main(int argc, char **argv) {
+    readArguments(argc, argv);
     return 0;
 }
