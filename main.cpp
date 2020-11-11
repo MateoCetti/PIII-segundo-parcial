3e66d001a9c63d7acda17eeb92f5ef43c9443229
Mateo P. Cetti <mateocetti@hotmail.com>
2020-11-11 15:15:58 -0300

    Verify accepted arguments

--- main.cpp
+++ main.cpp
@@ -5,16 +5,54 @@
 
 using namespace std;
 
+bool hasAuxiliar(char *nextArg){
+    if(nextArg[0] != '-'){
+        cout<< " Auxiliar: "<< nextArg<<"\n";
+        return 1;
+    }
+    return 0;
+}
+
+void verifyAviliableParameter(int i, char **argv){
+
+}
+
 void readArguments(int argc, char **argv){
-    cout << "Cantidad de argumentos: " << argc-1 << "\n";
     for (int i = 1; i < argc; i++){
         if(argv[i][0] == '-'){
-            cout << "Argumento " << i << " " << argv[i];
-            if(i < argc-1){
-                if(argv[i+1][0] != '-'){
-                    cout<< " Auxiliar: "<< argv[i+1]<<"\n";
-                    i++;
+            if (strcmp(argv[i], "-estad") == 0) {
+                cout << "Argumento " << i << " " << argv[i];
+            }
+            else if(strcmp(argv[i], "-p_casos")  == 0) {
+                cout << "Argumento " << i << " " << argv[i];
+                if(i < argc-1){
+                    bool x = hasAuxiliar(argv[i+1]);
+                    i+=x;
                 }
+            }
+            else if(strcmp(argv[i], "-p_muertes")  == 0) {
+                cout << "Argumento " << i << " " << argv[i];
+                if(i < argc-1){
+                    bool x = hasAuxiliar(argv[i+1]);
+                    i+=x;
+                }
+            }
+            else if(strcmp(argv[i], "-casos_edad")  == 0) {
+                cout << "Argumento " << i << " " << argv[i];
+                if(i < argc-1){
+                    bool x = hasAuxiliar(argv[i+1]);
+                    i+=x;
+                }
+            }
+            else if(strcmp(argv[i], "-casos_cui")  == 0) {
+                cout << "Argumento " << i << " " << argv[i];
+                if(i < argc-1){
+                    bool x = hasAuxiliar(argv[i+1]);
+                    i+=x;
+                }
+            }
+            else{
+                cout<<"Ingrese un argumento valido.\n";
             }
             cout<< "\n\n";
         }
