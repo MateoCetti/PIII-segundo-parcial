fecbc25c8f0dad499cd4d90010ecb5100f3a0f09
Mateo P. Cetti <mateocetti@hotmail.com>
2020-11-14 10:57:36 -0300

    p_casos and p_muertes done

--- main.cpp
+++ main.cpp
@@ -5,76 +5,37 @@
 #include "rsc/Caso.h"
 #include "rsc/ArgumentsManagement.h"
 #include "rsc/ReadCSV.h"
-#include "rsc/Provincia.h"
+#include "rsc/p_casos_muertes.h"
 using namespace std;
 
-void quickSort(Provincia *arr, int first, int last){
-    int i, j, middle;
-    Provincia pivot, aux;
-
-    middle = (first+last)/2;
-    pivot = arr[middle];
-    i=first;
-    j=last;
-
-    do{
-        while(arr[i].getCounter()>pivot.getCounter())i++;
-        while(arr[j].getCounter()<pivot.getCounter())j--;
-        if(i<=j){
-            aux= arr[i];
-            arr[i]=arr[j];
-            arr[j]= aux;
-            i++;
-            j--;
-        }
-    }while(i<=j);
-    if(j>first) quickSort(arr, first, j);
-    if(i< last) quickSort(arr, i, last);
-}
-
 int main(int argc, char **argv) {
+    printf("\033c");
     int* myArguments = readArguments(argc, argv);
     if(hasError(myArguments)){
         cout<<"\n\nHubo un error en el pasaje de argumentos! \n"
               "Por favor ingrese los argumentos correctamente.\n\n\n";
     }
     else{
-        cout<<"\n\n** Procesando datos **\n\n";
-        /* TODO manage data */
+        cout<<"\n** Procesando datos **\n\n";
         int casos = getCases();
         Caso *misCasos = new Caso[casos];
+
         Estad *estad = new Estad;
         exploreCSV(myArguments[0], estad, misCasos, casos);
-        /* 0.Buenos Aires 1.CABA 2.Catamarca 3.Chaco 4.Chubut 5.Córdoba 6.Corrientes 7.Entre Ríos
-        8.Formosa 9.Jujuy 10.La Pampa 11.La Rioja 12.Mendoza 13.Misiones 14. Neuquén
-        15.Río Negro 16.Salta 17.San Juan 18.San Luis 19.Santa Cruz 20.Santa Fe
-        21.Santiago del Estero 22.Tierra del Fuego 23.Tucumán*/
+
         if(myArguments[1] != -1){
-            cout<<"\n\n** Mostrando Contagios por provincia **\n\n";
-            string provincias[24] = {
-                    "Buenos Aires", "CABA", "Catamarca", "Chaco", "Chubut", "Córdoba", "Corrientes",
-                    "Entre Ríos", "Formosa", "Jujuy", "La Pampa", "La Rioja", "Mendoza", "Misiones",
-                    "Neuquén", "Río Negro", "Salta", "San Juan", "San Luis", "Santa Cruz", "Santa Fe",
-                    "Santiago del Estero", "Tierra del Fuego", "Tucumán"};
+            cout<<"\n\n** Mostrando contagios por provincia **\n\n";
             Provincia misProvincias[24];
-            for(int i=0; i<24;i++){
-                misProvincias[i].setName(provincias[i]);
-            }
-            for (int i = 0; i<casos; i++){
-                for(int j=0;j<24;j++){
-                    if(misCasos[i].getClasifResumen().compare("Confirmado")==0){
-                        if(misCasos[i].getProvincia().compare(provincias[j])==0){
-                            misProvincias[j].IncrementCounter();
-                            break;
-                        }
-                    }
-                }
-            }
+            p_casos_muertes(misProvincias, misCasos, casos, 'c');
             int n = myArguments[1] == 0 ? 24 : myArguments[1];
-            quickSort(misProvincias, 0,23);
-            for(int i=0; i<n; i++){
-                misProvincias[i].toString();
-            }
+            printP(misProvincias, n);
+        }
+        if(myArguments[2] != -1){
+            cout<<"\n\n** Mostrando fallecidos por provincia **\n\n";
+            Provincia misProvincias[24];
+            p_casos_muertes(misProvincias, misCasos, casos, 'f');
+            int n = myArguments[2] == 0 ? 24 : myArguments[1];
+            printP(misProvincias, n);
         }
     }
     return 0;
---
+++ rsc/p_casos_muertes.h
@@ -0,0 +1,71 @@
+#ifndef INC_2_PARCIAL_P_CASOS_MUERTES_H
+#define INC_2_PARCIAL_P_CASOS_MUERTES_H
+
+#include "Provincia.h"
+
+void quickSort(Provincia *arr, int first, int last){
+    int i, j, middle;
+    Provincia pivot, aux;
+
+    middle = (first+last)/2;
+    pivot = arr[middle];
+    i=first;
+    j=last;
+
+    do{
+        while(arr[i].getCounter()>pivot.getCounter())i++;
+        while(arr[j].getCounter()<pivot.getCounter())j--;
+        if(i<=j){
+            aux= arr[i];
+            arr[i]=arr[j];
+            arr[j]= aux;
+            i++;
+            j--;
+        }
+    }while(i<=j);
+    if(j>first) quickSort(arr, first, j);
+    if(i< last) quickSort(arr, i, last);
+}
+
+void p_casos_muertes(Provincia *misProvincias,Caso *misCasos, int casos, char p){
+    string provincias[24] = {
+            "Buenos Aires", "CABA", "Catamarca", "Chaco", "Chubut", "Córdoba", "Corrientes",
+            "Entre Ríos", "Formosa", "Jujuy", "La Pampa", "La Rioja", "Mendoza", "Misiones",
+            "Neuquén", "Río Negro", "Salta", "San Juan", "San Luis", "Santa Cruz", "Santa Fe",
+            "Santiago del Estero", "Tierra del Fuego", "Tucumán"};
+    for(int i=0; i<24;i++){
+        misProvincias[i].setName(provincias[i]);
+    }
+    if(p=='c'){
+        for (int i = 0; i<casos; i++){
+            for(int j=0;j<24;j++){
+                if(misCasos[i].getClasifResumen().compare("Confirmado")==0){
+                    if(misCasos[i].getProvincia().compare(provincias[j])==0){
+                        misProvincias[j].IncrementCounter();
+                        break;
+                    }
+                }
+            }
+        }
+    }else if(p=='f'){
+        for (int i = 0; i<casos; i++){
+            for(int j=0;j<24;j++){
+                if(misCasos[i].getFallecido().compare("SI")==0){
+                    if(misCasos[i].getProvincia().compare(provincias[j])==0){
+                        misProvincias[j].IncrementCounter();
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+
+void printP(Provincia *misProvincias, int n){
+    quickSort(misProvincias, 0,23);
+    for(int i=0; i<n; i++){
+        misProvincias[i].toString();
+    }
+}
+
+#endif //INC_2_PARCIAL_P_CASOS_MUERTES_H
